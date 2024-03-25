1b9057d0ee682e561d95955d151aaf83e880a536
srishti-23 <142992320+srishti-23@users.noreply.github.com>
2024-03-25 15:52:31 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import Imageswitcher from "./ImageSwitcher";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
@@ -6,7 +5,7 @@ import HomeContent from "./HomeContent";
 import OfferTime from "./OfferTime";
 
 const Home = () => {
-  const url = "https://fakestoreapi.com/products/";
+
 
   return (
     <>
